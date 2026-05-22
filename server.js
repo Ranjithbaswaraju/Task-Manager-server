35253108830de93e0bf00f49f50eabccddf591ae
Ranjithbaswaraj <ranjithbaswaraju10@gmail.com>
2026-05-22 09:53:52 +0530

    finalCommit

--- server.js
+++ server.js
@@ -1,53 +1,26 @@
 const express = require("express");
-
 const cors = require("cors");
-
 const ConnectDB = require("./DB_Configue/db");
-
 const userRoutes = require("./Routes/userRoutes");
-
 const TLRoutes = require("./Routes/TLroutes");
-
 const employeeRoutes = require("./Routes/employeeRoutes");
-
 const taskRoutes = require("./Routes/taskRoutes");
-
 const app = express();
-
 const port = 3000;
-
-
-
 // CORS
 app.use(cors({
     origin: "http://localhost:5173"
 }));
-
-
-
 // Middleware
 app.use(express.json());
-
 app.use(express.urlencoded({ extended: true }));
-
-
-
 // Database Connection
 ConnectDB();
-
-
-
 // Routes
 app.use("/api", userRoutes);
-
 app.use("/api", TLRoutes);
-
 app.use("/api", employeeRoutes);
-
 app.use("/api", taskRoutes);
-
-
-
 // Server
 app.listen(port, () => {
 
