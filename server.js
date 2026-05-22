42b1e1b54c86ce3c105348776f02cc3797ddd171
Ranjithbaswaraj <ranjithbaswaraju10@gmail.com>
2026-05-22 16:20:42 +0530

    verceladded

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 const port = 3000;
 // CORS
 app.use(cors({
-    origin: "http://localhost:5173"
+    origin: ["https://task-manager-client-kohl.vercel.app","http://localhost:5173"]
 }));
 // Middleware
 app.use(express.json());
